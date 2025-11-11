b294b391bf4f9af39d279931bed603530d49c0e6
Guilherme Beira <guilherme.vieira.beira@gmail.com>
2025-11-11 10:26:15 -0300

    Migrate from socket to websocket

--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,17 @@
-use bytes::{Bytes, BytesMut};
+use std::{net::SocketAddr, sync::Arc};
+
+use axum::{
+    Router,
+    extract::{
+        ConnectInfo, State,
+        ws::{Message as WsMessage, WebSocket, WebSocketUpgrade},
+    },
+    response::IntoResponse,
+    routing::get,
+};
 use dashmap::DashMap;
 use futures_util::{SinkExt, StreamExt};
-use std::{collections::HashSet, net::SocketAddr, sync::Arc};
-use tokio::{
-    net::{TcpListener, TcpStream},
-    sync::mpsc,
-};
-use tokio_util::codec::{Decoder, LengthDelimitedCodec};
+use tokio::{net::TcpListener, sync::mpsc};
 use uuid::Uuid;
 
 mod protocol;
@@ -14,7 +19,13 @@ use protocol::*;
 
 #[derive(Clone)]
 struct Worker {
-    channel: mpsc::Sender<Message>,
+    channel: mpsc::Sender<Outbound>,
+}
+
+#[derive(Debug)]
+enum Outbound {
+    Protocol(Message),
+    Raw(WsMessage),
 }
 
 struct Function {
@@ -43,23 +54,8 @@ impl Engine {
         }
     }
 
-    pub async fn listen(self: Arc<Self>, addr: &str) -> anyhow::Result<()> {
-        let listener = TcpListener::bind(addr).await?;
-        println!("Engine listening on {}", addr);
-
-        loop {
-            let (socket, peer) = listener.accept().await?;
-            let this = self.clone();
-            tokio::spawn(async move {
-                if let Err(e) = this.handle_worker(socket, peer).await {
-                    eprintln!("worker error: {e:?}");
-                }
-            });
-        }
-    }
-
     async fn send_msg(&self, worker: &Worker, msg: Message) -> bool {
-        worker.channel.send(msg).await.is_ok()
+        worker.channel.send(Outbound::Protocol(msg)).await.is_ok()
     }
 
     async fn router_msg(&self, worker: &Worker, msg: &Message) -> anyhow::Result<()> {
@@ -69,69 +65,53 @@ impl Engine {
                 function_path,
                 data,
             } => {
-                let function_option = self.functions.get(function_path);
-                match function_option {
-                    Some(function) => {
-                        function
-                            .worker
-                            .channel
-                            .send(Message::InvokeFunction {
-                                invocation_id: invocation_id.clone(),
-                                function_path: function_path.clone(),
-                                data: data.clone(),
-                            })
-                            .await?;
-
-                        return Ok(());
-                    }
-                    None => {
-                        self.send_msg(
-                            &worker,
-                            Message::InvocationResult {
-                                invocation_id: invocation_id.clone(),
-                                function_path: function_path.clone(),
-                                result: None,
-                                error: Some(ErrorBody {
-                                    code: "function_not_found".into(),
-                                    message: "Function not found".to_string(),
-                                }),
-                            },
-                        )
-                        .await;
-
-                        return Ok(());
-                    }
+                if let Some(function) = self.functions.get(function_path) {
+                    function
+                        .worker
+                        .channel
+                        .send(Outbound::Protocol(Message::InvokeFunction {
+                            invocation_id: *invocation_id,
+                            function_path: function_path.clone(),
+                            data: data.clone(),
+                        }))
+                        .await?;
+                } else {
+                    self.send_msg(
+                        worker,
+                        Message::InvocationResult {
+                            invocation_id: *invocation_id,
+                            function_path: function_path.clone(),
+                            result: None,
+                            error: Some(ErrorBody {
+                                code: "function_not_found".into(),
+                                message: "Function not found".to_string(),
+                            }),
+                        },
+                    )
+                    .await;
                 }
+                Ok(())
             }
-
             Message::InvocationResult {
                 invocation_id,
                 function_path,
                 result,
                 error,
             } => {
-                let invocation = self.invocations.get(invocation_id);
-                match invocation {
-                    Some(invocation) => {
-                        invocation
-                            .worker
-                            .channel
-                            .send(Message::InvocationResult {
-                                invocation_id: invocation_id.clone(),
-                                function_path: function_path.clone(),
-                                result: result.clone(),
-                                error: error.clone(),
-                            })
-                            .await?;
-                    }
-                    None => {
-                        // TODO let's see if we need to handle invocation result for
-                        // callee workers that were removed afterwards
-                    }
+                if let Some(invocation) = self.invocations.get(invocation_id) {
+                    invocation
+                        .worker
+                        .channel
+                        .send(Outbound::Protocol(Message::InvocationResult {
+                            invocation_id: *invocation_id,
+                            function_path: function_path.clone(),
+                            result: result.clone(),
+                            error: error.clone(),
+                        }))
+                        .await?;
                 }
-                return Ok(());
+                Ok(())
             }
-
             Message::RegisterFunction {
                 function_path,
                 description,
@@ -142,69 +122,111 @@ impl Engine {
                     _description: description.clone(),
                 };
                 self.functions.insert(function_path.clone(), new_function);
-
-                return Ok(());
+                Ok(())
             }
-
-            _ => {
-                return Err(anyhow::anyhow!("Unknown message: {:?}", msg));
+            Message::Ping => {
+                self.send_msg(worker, Message::Pong).await;
+                Ok(())
             }
+            Message::Pong => Ok(()),
         }
     }
 
     async fn handle_worker(
         self: Arc<Self>,
-        socket: TcpStream,
+        socket: WebSocket,
         peer: SocketAddr,
     ) -> anyhow::Result<()> {
-        let str_peer_addr = peer.to_string();
-        let codec = LengthDelimitedCodec::builder()
-            .max_frame_length(64 * 1024 * 1024) // 64 MiB, just for testing
-            .new_codec();
-        let framed = codec.framed(socket);
-        let (mut wr, mut rd) = framed.split::<Bytes>();
+        println!(">> Worker {peer} connected via WebSocket");
+        let (mut ws_tx, mut ws_rx) = socket.split();
+        let (tx, mut rx) = mpsc::channel::<Outbound>(64);
 
-        // Channel used to push outbound messages to this worker
-        let (tx, mut rx) = mpsc::channel::<Message>(64);
-
-        // Writer task
         let writer = tokio::spawn(async move {
-            while let Some(msg) = rx.recv().await {
-                let bytes = serde_json::to_vec(&msg).unwrap();
-                if let Err(e) = wr.send(Bytes::from(bytes)).await {
-                    eprintln!("send error: {e:?}");
+            while let Some(outbound) = rx.recv().await {
+                let send_result = match outbound {
+                    Outbound::Protocol(msg) => match serde_json::to_string(&msg) {
+                        Ok(payload) => ws_tx.send(WsMessage::Text(payload)).await,
+                        Err(err) => {
+                            eprintln!("serialize error: {err:?}");
+                            continue;
+                        }
+                    },
+                    Outbound::Raw(frame) => ws_tx.send(frame).await,
+                };
+
+                if send_result.is_err() {
                     break;
                 }
             }
         });
 
-        let worker = Worker { channel: tx };
+        let worker = Worker {
+            channel: tx.clone(),
+        };
 
-        // Reader loop
-        while let Some(frame) = rd.next().await {
-            let bytes: BytesMut = frame?;
-            let msg: Message = serde_json::from_slice(&bytes)?;
-            println!("Received message from worker {}: {:?}", str_peer_addr, msg);
-            self.router_msg(&worker, &msg).await?;
+        while let Some(frame) = ws_rx.next().await {
+            match frame {
+                Ok(WsMessage::Text(text)) => {
+                    if text.trim().is_empty() {
+                        continue;
+                    }
+                    match serde_json::from_str::<Message>(&text) {
+                        Ok(msg) => self.router_msg(&worker, &msg).await?,
+                        Err(err) => eprintln!("json decode error from {peer}: {err}"),
+                    }
+                }
+                Ok(WsMessage::Binary(bytes)) => match serde_json::from_slice::<Message>(&bytes) {
+                    Ok(msg) => self.router_msg(&worker, &msg).await?,
+                    Err(err) => eprintln!("binary decode error from {peer}: {err}"),
+                },
+                Ok(WsMessage::Close(_)) => break,
+                Ok(WsMessage::Ping(payload)) => {
+                    let _ = tx.send(Outbound::Raw(WsMessage::Pong(payload))).await;
+                }
+                Ok(WsMessage::Pong(_)) => {}
+                Err(err) => {
+                    eprintln!("websocket error from {peer}: {err}");
+                    break;
+                }
+            }
         }
 
         writer.abort();
-        println!(">> Worker {} disconnected", str_peer_addr);
-
+        println!(">> Worker {peer} disconnected");
         Ok(())
     }
 }
 
+async fn ws_handler(
+    State(engine): State<Arc<Engine>>,
+    ws: WebSocketUpgrade,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    ws.on_upgrade(move |socket| {
+        let engine = engine.clone();
+        async move {
+            if let Err(err) = engine.handle_worker(socket, addr).await {
+                eprintln!("worker error: {err:?}");
+            }
+        }
+    })
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
     let engine = Arc::new(Engine::new());
-    let eng = engine.clone();
+    let app = Router::new().route("/", get(ws_handler)).with_state(engine);
 
-    tokio::spawn(async move {
-        eng.listen("127.0.0.1:49134").await.unwrap();
-    });
+    let addr = "127.0.0.1:49134";
+    let listener = TcpListener::bind(addr).await?;
+    println!("Engine listening on ws://{addr}/");
 
-    loop {
-        tokio::time::sleep(std::time::Duration::from_secs(60)).await;
-    }
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await?;
+    Ok(())
 }
