9780aff81e562f620632ca190a81791955bc7dae
Guilherme Beira <guilherme.vieira.beira@gmail.com>
2025-11-14 10:29:14 -0300

    Ensure no dead lock

--- src/server.rs
+++ src/server.rs
@@ -62,7 +62,7 @@ struct Function {
 
 #[derive(Default)]
 struct Engine {
-    worker_registry: WorkerRegistry,
+    worker_registry: Arc<RwLock<WorkerRegistry>>,
     functions: Arc<DashMap<String, Function>>,
     trigger_registry: Arc<TriggerRegistry>,
     service_registry: Arc<RwLock<ServicesRegistry>>,
@@ -72,7 +72,7 @@ struct Engine {
 impl Engine {
     fn new() -> Self {
         Self {
-            worker_registry: WorkerRegistry::new(),
+            worker_registry: Arc::new(RwLock::new(WorkerRegistry::new())),
             functions: Arc::new(DashMap::new()),
             trigger_registry: Arc::new(TriggerRegistry::new()),
             pending_invocations: Arc::new(RwLock::new(DashMap::new())),
@@ -116,7 +116,7 @@ impl Engine {
     async fn halt_invocation(&self, invocation_id: &Uuid) {
         tracing::info!(invocation_id = %invocation_id, "Halting invocation");
         if let Some((_, worker_id)) = self.pending_invocations.write().await.remove(invocation_id)
-            && let Some(worker_ref) = self.worker_registry.get_worker(&worker_id)
+            && let Some(worker_ref) = self.worker_registry.read().await.get_worker(&worker_id)
         {
             worker_ref.halt_invocation(invocation_id).await;
         }
@@ -148,6 +148,7 @@ impl Engine {
     }
 
     async fn generate_functions_hash(&self) -> String {
+        tracing::info!("Generating functions hash");
         let functions: HashSet<String> = self
             .functions
             .iter()
@@ -160,7 +161,7 @@ impl Engine {
     }
 
     async fn broadcast_msg(&self, msg: Message) {
-        for worker in self.worker_registry.workers.iter() {
+        for worker in self.worker_registry.read().await.workers.iter() {
             let _ = worker
                 .value()
                 .channel
@@ -178,7 +179,7 @@ impl Engine {
             .write()
             .await
             .remove(invocation_id)?;
-        let worker_ref = self.worker_registry.get_worker(&worker_id)?;
+        let worker_ref = self.worker_registry.read().await.get_worker(&worker_id)?;
         let sender = worker_ref.channel.clone();
         let invocation = worker_ref
             .invocations
@@ -477,7 +478,10 @@ impl Engine {
         let worker = Worker::new(tx.clone());
 
         tracing::info!(worker_id = %worker.id, peer = %peer, "Assigned worker ID");
-        self.worker_registry.register_worker(worker.clone());
+        self.worker_registry
+            .write()
+            .await
+            .register_worker(worker.clone());
 
         while let Some(frame) = ws_rx.next().await {
             match frame {
@@ -527,7 +531,10 @@ impl Engine {
         }
 
         self.trigger_registry.unregister_worker(&worker.id).await;
-        self.worker_registry.unregister_worker(&worker.id);
+        self.worker_registry
+            .write()
+            .await
+            .unregister_worker(&worker.id);
 
         let worker_invocations = worker.invocations.read().await;
 
