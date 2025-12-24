c3a191c68e5d83cc8b2a4b8ee4d8db71b33fa52b
Ytallo Layon <ytallo.layon@gmail.com>
2025-12-24 11:45:37 -0300

    refactor: optimize dev function for concurrent type generation and build

--- packages/node/motia/src/new/build/dev.ts
+++ packages/node/motia/src/new/build/dev.ts
@@ -3,23 +3,24 @@ import { generateIndex } from './generate-index'
 import { typegen } from './typegen'
 
 export const dev = async () => {
-  const stopTypegen = await typegen({ watch: true, silent: false })
-
-  const result = await esbuild.build({
-    stdin: {
-      contents: generateIndex(),
-      sourcefile: 'index-dev.js',
-      resolveDir: process.cwd(),
-      loader: 'js',
-    },
-    packages: 'external',
-    platform: 'node',
-    target: ['node22'],
-    format: 'esm',
-    bundle: true,
-    sourcemap: true,
-    outfile: 'dist/index-dev.js',
-  })
+  const [_, result] = await Promise.all([
+    typegen({ watch: true, silent: false }),
+    esbuild.build({
+      stdin: {
+        contents: generateIndex(),
+        sourcefile: 'index-dev.js',
+        resolveDir: process.cwd(),
+        loader: 'js',
+      },
+      packages: 'external',
+      platform: 'node',
+      target: ['node22'],
+      format: 'esm',
+      bundle: true,
+      sourcemap: true,
+      outfile: 'dist/index-dev.js',
+    })
+  ])
 
   return result
 }
