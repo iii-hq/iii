c150b54a98a92ee89f32d196a436a8b1499473e9
Sergio Marcelino <sergio@filho.org>
2026-01-15 09:58:49 -0300

    fix: clippy issue

--- src/modules/state/config.rs
+++ src/modules/state/config.rs
@@ -2,15 +2,9 @@ use serde::{Deserialize, Serialize};
 
 use crate::modules::core_module::AdapterEntry;
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
 #[serde(deny_unknown_fields)]
 pub struct StateModuleConfig {
     #[serde(default)]
     pub adapter: Option<AdapterEntry>,
-}
-
-impl Default for StateModuleConfig {
-    fn default() -> Self {
-        Self { adapter: None }
-    }
 }
