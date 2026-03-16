c91af4688b754c86da861a7b1296f53a65047b58
Sergio Marcelino <sergio@filho.org>
2026-03-16 07:46:12 -0300

    feat: mintlify

--- docs/src/lib/layout.shared.tsx
+++
@@ -1,8 +0,0 @@
-import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
-import { NavTitle } from "@/lib/components/NavTitle";
-
-export function baseOptions(): BaseLayoutProps {
-  return {
-    nav: { title: <NavTitle />, url: "/docs" },
-  };
-}
